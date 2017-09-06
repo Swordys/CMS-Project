0ad39ef400cdd940f3acb5ee3e4b58d64b8dbe7b
David <davit_saginashvili@yahoo.com>
2017-09-06 04:22:22 +0400

    added emoji box transitions

--- src/components/message/messageLog/EmojiBox.jsx
+++ src/components/message/messageLog/EmojiBox.jsx
@@ -1,30 +1,66 @@
 import React from "react";
 import { Picker } from "emoji-mart";
 import { connect } from "react-redux";
+import Transition from "react-transition-group/Transition";
 // CSS
 import "./css/emoji-mart.css";
 
 const EmojiBox = props => {
   const { showEmoji } = props;
-  let retVal = null;
-  if (showEmoji) {
-    retVal = (
-      <div className="emojiWrap">
-        <Picker
-          set="emojione"
-          exclude={["recent"]}
-          emojiSize={23}
-          perLine={8}
-          title={""}
-          color={"rgba(0, 91, 234, 0.9)"}
-          onClick={(emoji, e) => {
-            console.log(emoji);
-          }}
-        />
-      </div>
-    );
-  }
-  return retVal;
+
+  const duration = 200;
+
+  const defaultStyle = {
+    transition: `all ${duration}ms ease-out`,
+    opacity: 0,
+    transform: "scale(0.8)",
+    zIndex: -500
+  };
+
+  const transitionStyles = {
+    entering: {
+      opacity: 1,
+      transform: "scale(1)",
+      zIndex: "auto"
+    },
+    entered: {
+      opacity: 1,
+      transform: "scale(1)",
+      zIndex: "auto"
+    }
+  };
+
+  const emojiValue = (
+    <Picker
+      set="emojione"
+      exclude={["recent"]}
+      emojiSize={23}
+      perLine={8}
+      title={""}
+      color={"rgba(0, 91, 234, 0.9)"}
+      onClick={(emoji, e) => {
+        console.log(emoji);
+      }}
+    />
+  );
+
+  return (
+    <Transition in={showEmoji} timeout={duration}>
+      {state => {
+        return (
+          <div
+            className="emojiWrap"
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}
+          >
+            {emojiValue}
+          </div>
+        );
+      }}
+    </Transition>
+  );
 };
 
 const mapStateToProps = state => ({
