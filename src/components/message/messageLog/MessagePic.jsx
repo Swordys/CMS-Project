be48a964fd94e7e9eb33873b72f532155baeab31
David <davit_saginashvili@yahoo.com>
2017-09-22 02:15:28 +0400

    decorations

--- src/components/message/messageLog/MessagePic.jsx
+++ src/components/message/messageLog/MessagePic.jsx
@@ -3,22 +3,30 @@ import { connect } from "react-redux";
 
 const MessagePic = props => {
   const { msgLog } = props;
-  const len = msgLog.length - 1;
-  console.log(len);
+  let len = msgLog.length - 1;
+
   const picStyle = {
     height: "35px",
     width: "35px",
     borderRadius: "50%",
-    background: "gray",
+    background: "white",
     position: "absolute",
     right: "-45px",
     bottom: "0",
     transform: `translateY(${len * 43}px)`,
     transition: "transform 300ms cubic-bezier(.67,.35,.34,.91) 100ms"
   };
+
+  const picImage = {
+    backgroundImage:
+      "url(https://raw.githubusercontent.com/Reactive-Extensions/RxJS/master/doc/designguidelines/images/984368.png)",
+    height: "100%",
+    width: "100%",
+    backgroundSize: "contain"
+  };
   return (
     <div style={picStyle}>
-      <div />
+      <div style={picImage} />
     </div>
   );
 };
