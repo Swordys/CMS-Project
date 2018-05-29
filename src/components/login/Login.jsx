bc10d0b1688905f2e400dc71d8d375e517fb6f6b
Swordys <davit_saginashvili@yahoo.com>
2018-05-29 20:37:21 +0400

    Login modal tweaks

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import firebase from "../../firebase/firestoreAuth";
 
 import "../../css/messageApp/login/login.css";
 
-const Login = class extends React.Component {
+const Login = class extends Component {
   state = {
     phoneNumber: "",
     confirmCode: "",
@@ -60,65 +60,78 @@ const Login = class extends React.Component {
 
   render() {
     return (
-      <div className="messenger-login-wrap">
-        <div
-          style={{
-            transform: `${
-              this.state.numberConfirm
-                ? "translate3d(0, 0, 0)"
-                : "translate3d(0, 55px, 0)"
-            }`
-          }}
-          className="messenger-login-form-wrap"
-        >
-          <form
-            className="messenger-login-form"
-            onSubmit={this.handleSubmitNumber}
+      <div className="messenger-login-page-wrap">
+        <div className="messenger-login-wrap">
+          <div
+            style={{
+              transform: `${
+                this.state.numberConfirm
+                  ? "translate3d(0, 0, 0)"
+                  : "translate3d(0, 55px, 0)"
+              }`
+            }}
+            className="messenger-login-banner"
+          />
+          <div
+            style={{
+              transform: `${
+                this.state.numberConfirm
+                  ? "translate3d(0, 0, 0)"
+                  : "translate3d(0, 55px, 0)"
+              }`
+            }}
+            className="messenger-login-form-wrap"
           >
-            <label htmlFor="input-number">
-              SIGN IN
-              <input
-                placeholder="Mobile number"
-                id="input-number"
-                className="login-form-input"
-                value={this.state.phoneNumber}
-                onChange={({ target }) => {
-                  this.setState({ phoneNumber: target.value });
-                }}
-                type="text"
-              />
-            </label>
-          </form>
-        </div>
-        <div
-          style={{
-            transform: `${
-              this.state.numberConfirm
-                ? "translate3d(0, 0, 0)"
-                : "translate3d(0, -55px, 0)"
-            }`,
-            zIndex: `${this.state.numberConfirm ? "1" : "-1"}`
-          }}
-          className="messenger-login-form-wrap"
-        >
-          <form
-            className="messenger-login-form"
-            onSubmit={this.handleConfirmCode}
+            <form
+              className="messenger-login-form"
+              onSubmit={this.handleSubmitNumber}
+            >
+              <label htmlFor="input-number">
+                SIGN IN
+                <input
+                  placeholder="Mobile Number"
+                  id="input-number"
+                  className="login-form-input"
+                  value={this.state.phoneNumber}
+                  onChange={({ target }) => {
+                    this.setState({ phoneNumber: target.value });
+                  }}
+                  type="text"
+                />
+              </label>
+            </form>
+          </div>
+          <div
+            style={{
+              transform: `${
+                this.state.numberConfirm
+                  ? "translate3d(0, 0, 0)"
+                  : "translate3d(0, -55px, 0)"
+              }`,
+              opacity: `${this.state.numberConfirm ? "1" : "0"}`
+            }}
+            className="messenger-login-form-wrap"
           >
-            <label htmlFor="input-number">
-              VERIFY
-              <input
-                placeholder="Verification code"
-                id="input-code"
-                className="login-form-input"
-                value={this.state.confirmCode}
-                onChange={({ target }) => {
-                  this.setState({ confirmCode: target.value });
-                }}
-                type="text"
-              />
-            </label>
-          </form>
+            <form
+              className="messenger-login-form"
+              onSubmit={this.handleConfirmCode}
+            >
+              <label htmlFor="input-number">
+                VERIFY
+                <input
+                  readOnly={!this.state.numberConfirm}
+                  placeholder="Verification Code"
+                  id="input-code"
+                  className="login-form-input"
+                  value={this.state.confirmCode}
+                  onChange={({ target }) => {
+                    this.setState({ confirmCode: target.value });
+                  }}
+                  type="text"
+                />
+              </label>
+            </form>
+          </div>
         </div>
         <div id="sign-in-button" />
       </div>
--- src/firebase/firestoreAuth.js
+++ src/firebase/firestoreAuth.js
@@ -1,4 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
+firebase.auth().settings.appVerificationDisabledForTesting = true;
+
 export default firebase;
